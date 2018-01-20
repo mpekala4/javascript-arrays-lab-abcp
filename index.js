777949803acfece505979a907a986b3c80afd0df
Mike Pekala <mpekala411@gmail.com>
2018-01-20 01:58:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(array, element){
+  kittens.push("Ralph");
+  return kittens
+}
+
+function destructivelyPrependKitten(array, element){
+  kittens.unshift("Bob");
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(array, index){
+  kittens.pop(index-1)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(array, index){
+  kittens.shift(0)
+  return kittens
+}
+
+function appendKitten(array, element){
+  return[...kittens, "Broom"]
+  appendKitten()
+}
+
+function prependKitten(array, element){
+  return["Arnold",...kittens]
+  prependKitten()
+}
+
+function removeLastKitten(array,index){
+  var lastindexNum = kittens.length-1
+  return kittens.slice(0,lastindexNum)
+}
+
+function removeFirstKitten(array, index){
+  return kittens.slice(1)
+}
