1d4883170cfe6fd9478b931612c0f741145a03df
Mike Pekala <mpekala411@gmail.com>
2018-01-20 01:35:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, element){
   kittens.push("luna");
-  return kittens 
+  return kittens
+}
+
+function destructivelyPrependKitten(array, element){
+  kittens.unshift("tuna");
+  return kittens
 }
