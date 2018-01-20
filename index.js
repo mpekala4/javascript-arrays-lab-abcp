45837a5509bbdd6e26ba4358de280e2ece92da96
Mike Pekala <mpekala411@gmail.com>
2018-01-20 01:36:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(array, element){
-  kittens.push("luna");
+  kittens.push("Ralph");
   return kittens
 }
 
